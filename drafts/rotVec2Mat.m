e5c57785760595e509995e9de7b9561a1e0e72ef
Arash Hashemi <arshemii1373.2@gmail.com>
2025-01-26 17:50:02 +0100

    Delete drafts directory

--- drafts/rotVec2Mat.m
+++
@@ -1,8 +0,0 @@
-function [rotMat] = rotVect2Mat(W)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-rotMat = [0   -W(3)   0
-           W(3)    0    -W(3)
-           -W(2)    W(1)     0];
-end
-
